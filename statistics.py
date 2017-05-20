b835aee9260a86ff6a9ef46e0be17eec730355b6
Junbang Huang <wongzeonbong@gmail.com>
2017-05-20 00:37:04 -0400

    statistics

--- statistics.py
+++ statistics.py
@@ -4,18 +4,17 @@ from random_circle_removal import random_circle_removal
 from gradings import get_gradings
 from PageRank import page_rank
 from EM import em
+from borda_ordering import borda_ordering
 
 def th(ranking, th):
+    limit = int(len(ranking) * th)
+    ranking = list(filter(lambda x: x <= limit, ranking))
     rlen = len(ranking)
-    limit = int(rlen * th)
     cnt = 0
     total = 0
+
     for i in range(rlen):
-        if ranking[i] > limit:
-            continue
         for j in range(i + 1, rlen):
-            if ranking[j] > limit:
-                continue
             total = total + 1
             if ranking[i] < ranking[j]:
                 cnt = cnt + 1
@@ -57,6 +56,13 @@ if __name__ == '__main__':
     acc5.append(acc(ranking, 0.05))
 
     ranking = em(gradings)
+    all2all.append(th(ranking, 1))
+    th10.append(th(ranking, 0.1))
+    th50.append(th(ranking, 0.5))
+    acc2.append(acc(ranking, 0.02))
+    acc5.append(acc(ranking, 0.05))
+
+    ranking = borda_ordering(gradings)
     all2all.append(th(ranking, 1))
     th10.append(th(ranking, 0.1))
     th50.append(th(ranking, 0.5))
