067eb36bb75fcf45daf827dbfbe1853803816e78
Haoyue Ping <timoping@gmail.com>
2017-06-03 01:39:25 -0400

    Update PageRank. Now it works for large input dataset, but is still much slower than board ordering.

--- algorithms/PageRank.py
+++ algorithms/PageRank.py
@@ -1,48 +1,43 @@
 import numpy as np
 import pandas as pd
-from numpy.linalg import svd
 from scipy.stats import kendalltau
+from scipy.linalg import eig
 
 
 def page_rank(short_rankings):
     n, k = short_rankings.shape
-
     P = np.zeros((n, n))
     for ranking in short_rankings:
-        for i in range(k-1):
-            better_paper = ranking[i]
-            worse_paper = ranking[i+1]
-            P[worse_paper - 1][better_paper - 1] += 1
+        for i in range(k):
+            for j in range(i + 1, k):
+                better_paper = ranking[i]
+                worse_paper = ranking[j]
+                P[worse_paper - 1][better_paper - 1] += 1
 
-    P_super = np.ones_like(P) / n
-    P_super -= np.eye(n) / n
+    P += (np.ones_like(P) - np.eye(n)) / n
+    P /= P.sum(axis=1)[:, None]
 
-    P_final = P + P_super
-    for i in range(n):
-        P_final[i] = P_final[i] / P_final[i].sum()
-
-    U, s, V = svd(P_final, full_matrices=False)
-    S = np.diag(s)
     while True:
-        print('new round')
-        P_temp = str(P_final)
-        S = np.power(S, 2)
-        P_final = U.dot(S).dot(V)
-        if P_temp == str(P_final):
+        print('new round.')
+        P_temp = str(P)
+        P = P.dot(P)
+        P /= P.sum(axis=1)[:, None]
+        if P_temp == str(P):
             break
 
-    global_scores = pd.Series(P_final[0])
+    global_scores = pd.Series(P[0])
     global_ranking = np.array(global_scores.sort_values(ascending=False).index) + 1
     return global_ranking
 
 
 if __name__ == '__main__':
-
     n = 10000
     truth_ranking = list(range(1, n + 1))
 
     rankings = np.genfromtxt('../data/data_n_{}_k_6.csv'.format(n), delimiter=',', dtype='int')
+    from time import time
+    t = time()
     global_ranking = page_rank(rankings)
-
+    print('time = {}'.format(time()-t))
     accuracy = kendalltau(truth_ranking, global_ranking)
     print(accuracy)
