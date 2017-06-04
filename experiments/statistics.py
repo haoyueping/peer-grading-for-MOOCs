434add66741402a394289b92ec767f9ff6160962
Junbang Huang <wongzeonbong@gmail.com>
2017-06-04 17:10:48 -0400

    result

--- experiments/statistics.py
+++ experiments/statistics.py
@@ -5,7 +5,8 @@ from algorithms.PageRank import page_rank
 from algorithms.borda_ordering import borda_ordering
 from algorithms.random_circle_removal import random_circle_removal
 from utils.gradings import get_gradings
-
+from scipy.stats import kendalltau
+from time import time
 
 def th(ranking, fraction):
     """The total number of correctly recovered relations between pairs that include an exam paper
@@ -51,28 +52,39 @@ def acc(ranking, fraction):
 
 
 if __name__ == '__main__':
-    gradings = get_gradings(1000, 6)
-    m = 10
+    # n, k, rep, algo, time, distance, ranking
+    import pandas as pd
+    rep = 10
+    n = 1000
+    k = 6
     algos = [random_circle_removal, page_rank, em, borda_ordering]
+    algo_names = ['random_circle_removal', 'page_rank', 'em', 'borda_ordering']
     all2alls = [0 for x in algos]
     th10s = [0 for x in algos]
     th50s = [0 for x in algos]
     acc2s = [0 for x in algos]
     acc5s = [0 for x in algos]
-    for i in range(m):
+    dataSet = []
+    for i in range(rep):
+        gradings = get_gradings(n, k)
         for j in range(len(algos)):
+            start = time()
             ranking = algos[j](gradings)
+            duration = time() - start
+            dataSet.append([n, k, i, algo_names[j], duration, kendalltau([x + 1 for x in range(n)], ranking)[0], str(ranking)])
             all2alls[j] += th(ranking, 1)
             th10s[j] += th(ranking, 0.1)
             th50s[j] += th(ranking, 0.5)
             acc2s[j] += acc(ranking, 0.02)
             acc5s[j] += acc(ranking, 0.05)
-    all2alls = [x / m for x in all2alls]
-    th10s = [x / m for x in th10s]
-    th50s = [x / m for x in th50s]
-    acc2s = [x / m for x in acc2s]
-    acc5s = [x / m for x in acc5s]
+    all2alls = [x / rep for x in all2alls]
+    th10s = [x / rep for x in th10s]
+    th50s = [x / rep for x in th50s]
+    acc2s = [x / rep for x in acc2s]
+    acc5s = [x / rep for x in acc5s]
 
+    df = pd.DataFrame(dataSet, columns = ['n', 'k', 'rep', 'algo', 'time', 'distance', 'ranking'])
+    df.to_csv('results.csv', index = False)
     print(all2alls)
     print(th10s)
     print(th50s)
