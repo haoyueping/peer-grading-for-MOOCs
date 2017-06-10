49dcc1c9e492d47e73b5fb720cacbe5dcc6b1946
Paras Wadekar <psw36@drexel.edu>
2017-06-10 17:09:20 -0400

    read csv k=5 n=1000

---
+++ experiments/convergence.py
@@ -0,0 +1,57 @@
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+
+def extract_distance(filename):
+    datafile = csv.reader(open(filename, newline=''), delimiter=',')
+    next(datafile,None)
+    algo_list=['random_circle_removal','page_rank','em','borda_ordering']
+    array_distance=np.zeros((0,4))
+    dist=np.zeros((1,4))
+    for row in datafile:
+        algo_index=algo_list.index(row[3])
+        dist[0,algo_index]=float(row[5])
+        if algo_index==3:
+            array_distance=np.vstack((array_distance,dist))
+    
+    return array_distance
+
+def create_plot(distances_all):
+    num_algos=distances_all.shape[1]
+    ylist=[]
+    for j in range(num_algos):
+        distances=distances_all[:,j].flatten()
+        y=np.zeros((0,1))
+        x=np.zeros((0,1))
+        
+        m=[1,2,5,10,25,50,100]
+        for i in m:
+            dist=distances.reshape(i,-1)
+    #        print (dist.shape)
+            c=dist.mean(axis=0).reshape(-1,1)
+            
+            x=np.vstack((x,np.repeat(i,c.shape[0]).reshape(-1,1)))
+            y=np.vstack((y,c))
+        ylist.append(y)
+    fig = plt.figure()
+    
+    ax = plt.gca()
+    a=5
+    b=10
+    ax.scatter(x,ylist[0],color='c',alpha=0.4, label='Random Priority')
+    ax.scatter(x*0.98,ylist[1],color='r',alpha=0.4, label='Page Rank')
+    ax.scatter(x*1.02,ylist[2],color='b',alpha=0.4, label='EM')
+    ax.scatter(x,ylist[3],color='g',alpha=0.4, label='Borda Score')
+    ax.set_xscale('log')
+    ld=plt.legend(loc='upper left',prop={'size':16})
+    for handle in ld.legendHandles:
+        handle.set_sizes([150.0])
+    ax.set_xticks(m)    
+    ax.set_xticklabels(m)
+        
+if __name__ == '__main__':
+    distances=extract_distance('../out/results_n_1000_k_[5].csv')
+    distances=distances[:1000,:]
+    print (distances.shape)
+    create_plot(distances)
+    
---
+++ experiments/convergence_backup.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 10 14:44:23 2017
+
+@author: paras
+"""
+
+import csv
+import numpy as np
+
+spamReader = csv.reader(open('../out/results_n_1000_k_[5].csv', newline=''), delimiter=',')
+
+next(spamReader,None)
+#row1 = next(spamReader)
+#print (row1[0])
+n=1000#int(float(row1[0]))
+#==============================================================================
+# rcr_rankings=np.zeros((0,n)).astype(np.int)
+# pg_rankings=np.zeros((0,n)).astype(np.int)
+# em_rankings=np.zeros((0,n)).astype(np.int)
+# bo_rankings=np.zeros((0,n)).astype(np.int)
+# array_rankings=[rcr_rankings,pg_rankings,em_rankings,bo_rankings]
+#==============================================================================
+algo_list=['random_circle_removal','page_rank','em','borda_ordering']
+array_distance=np.zeros((0,4))
+dist=np.zeros((1,4))
+#==============================================================================
+# stop=10000
+#==============================================================================
+i=0
+for row in spamReader:
+#==============================================================================
+#     stop-=1
+#     if stop<0:
+#         break
+#==============================================================================
+    algo_index=algo_list.index(row[3])
+    dist[0,algo_index]=float(row[5])
+    if algo_index==3:
+        array_distance=np.vstack((array_distance,dist))
+#    array_distance[i%4]
+#    i+=1
+    
+#==============================================================================
+#     row[6]=row[6].replace('[','')
+#     row[6]=row[6].replace(']','')
+#     row[6]=row[6].replace('\n',' ')
+#     row[6]=row[6].replace(',',' ')
+#     ranking = np.fromstring(row[6], dtype=int, sep=' ')
+#     array_rankings[algo]=np.vstack((array_rankings[algo],ranking))
+#==============================================================================
+
+#==============================================================================
+# for arr in array_rankings:
+#     print (arr.shape)
+#==============================================================================
+
+print (array_distance.shape)
